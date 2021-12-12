f0f27bb3ca217f9eccfcddb8914660987b0151be
Abhiram <abhiram.satha@gmail.com>
2021-12-12 21:19:41 +0000

    Creating a new article for past tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,17 @@
-const textbox = document.getElementById('tweet-text');
-const charCounter = document.getElementById('char-counter');
-
 $(document).ready(function() {
-  let counter = 10;
-  $(charCounter).val(counter);
-  textbox.addEventListener('input', function(event) {
-    if (event.data) {
-      counter--;
-    } else if (!event.data) {
-      counter++;
-    }
-    if(counter < 0) {
-      $(charCounter).css('color', '#ff0000');
-    }
-    $(charCounter).val(counter);
-    })
+  let counter = 140;
+  $('#char-counter').val(counter)
+   $('#tweet-text').on('input', function(event) {
+    console.log(event);
+    $('#char-counter').val(counter - $(this).val().length);
+    if($(this).val().length > 140) {
+      $('#char-counter').css('color', '#ff0000');
+     } else {
+       $('#char-counter').css('color', '#545149');
+       
+     }
+     
     
-});
+     })
+     
+ });
