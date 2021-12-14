0b1b4c88bad47c617156dbd8de23fdd0607ade4c
Abhiram <abhiram.satha@gmail.com>
2021-12-14 03:40:54 +0000

    Adding timeago as well as user generated tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,28 +4,28 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
+  // {
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": "https://i.imgur.com/73hZDYK.png"
+  //     ,
+  //     "handle": "@SirIsaac"
+  //   },
+  //   "content": {
+  //     "text": "If I have seen further it is by standing on the shoulders of giants"
+  //   },
+  //   "created_at": 1461116232227
+  // },
+  // {
+  //   "user": {
+  //     "name": "Descartes",
+  //     "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //     "handle": "@rd" },
+  //   "content": {
+  //     "text": "Je pense , donc je suis"
+  //   },
+  //   "created_at": 1461113959088
+  // }
 ]
 
 $(document).ready(function() {
@@ -58,7 +58,7 @@ $(document).ready(function() {
         </header>
         <p>${text}</p>
         <footer class="profile-footer">
-          <span>${dateCreated}</span>
+          <span>${timeago.format(dateCreated)}</span>
           <div class="tweet-icons">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -79,13 +79,22 @@ $(document).ready(function() {
       event.preventDefault();
 
       $.post('/tweets', newTweet, function(event) {
-        console.log(event);
+        console.log(newTweet);
 
         
       })
-      .fail(function() {
-        console.log('error');
+    })
+
+    const loadTweets = $(function() {
+      const $button = $('.tweet-button');
+      $button.on('click', function() {
+        $.ajax('/tweets', {method: 'GET'})
+        .then(function (morePostsHtml) {
+          console.log(morePostsHtml);
+          renderTweets(morePostsHtml);
+        })
       })
     })
+    //renderTweets(loadTweets())
 })
 
