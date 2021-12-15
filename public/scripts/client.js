f75261b9e7646839a89ded4bf706d563b14c28a4
Abhiram <abhiram.satha@gmail.com>
2021-12-15 00:12:56 +0000

    Adding controls to make sure null tweets, 0 characters and tweets over 140 can not be tweeted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,52 +3,27 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  // {
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": "https://i.imgur.com/73hZDYK.png"
-  //     ,
-  //     "handle": "@SirIsaac"
-  //   },
-  //   "content": {
-  //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   },
-  //   "created_at": 1461116232227
-  // },
-  // {
-  //   "user": {
-  //     "name": "Descartes",
-  //     "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //     "handle": "@rd" },
-  //   "content": {
-  //     "text": "Je pense , donc je suis"
-  //   },
-  //   "created_at": 1461113959088
-  // }
-]
+const data = [];
 
-$(document).ready(function() {
-  const renderTweets = function(tweets) {
-      for (const userData of tweets) {
-        const tweetInfo = {
-          name: userData.user.name,
-          profileName: userData.user.handle,
-          avatar: userData.user.avatars,
-          dateCreated: userData.created_at,
-          text: userData.content.text,
-
-        }
-        $('#tweets-container').append(createTweetElement(tweetInfo));
-      }
+$(document).ready(function () {
+  const renderTweets = function (tweets) {
+    for (const userData of tweets) {
+      const tweetInfo = {
+        name: userData.user.name,
+        profileName: userData.user.handle,
+        avatar: userData.user.avatars,
+        dateCreated: userData.created_at,
+        text: userData.content.text,
+      };
+      $("#tweets-container").append(createTweetElement(tweetInfo));
     }
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-    const createTweetElement = function(tweet) {
-      const {avatar, profileName, text, dateCreated, name} = tweet
-      let $tweet = 
-      `<article class="tweet-box">
+  };
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  const createTweetElement = function (tweet) {
+    const { avatar, profileName, text, dateCreated, name } = tweet;
+    let $tweet = `<article class="tweet-box">
         <header class="profile-header">
           <div class="profile-link">
             <img class="profile-logo" src=${avatar}/>
@@ -65,36 +40,59 @@ $(document).ready(function() {
             <i class="fa-solid fa-heart"></i>
           </div>
         </footer>
-      </article>`
+      </article>`;
     /* Your code for creating the tweet element */
-      // ...
-      return $tweet;
-    }
-    
-    renderTweets(data);
+    // ...
+    return $tweet;
+  };
 
-    const $form = $('form')
-    $form.on('submit', function () {
-      const newTweet = $(this).serialize();
+  //renderTweets(data);
+
+  //const $form = $("form");
+
+  // $form.on('submit', function () {
+
+  //   event.preventDefault();
+  //   const newTweet = $(this).serialize();
+
+  //   $.post('/tweets', newTweet, function(event) {
+
+  //   })
+  // })
+
+  // const loadTweets = $(function() {
+  //   const $button = $('.tweet-button');
+  //   $button.on('click', function() {
+  //     $.ajax('/tweets', {method: 'GET'})
+  //     .then(function (morePostsHtml) {
+  //       renderTweets(morePostsHtml);
+  //     })
+  //   })
+  // })
+
+  // renderTweets();
+
+  const loadTweets = function () {
+    $.ajax("/tweets", { method: "GET" }).then(function (morePostsHtml) {
+      console.log(morePostsHtml);
+      renderTweets(morePostsHtml);
+    });
+  };
+  loadTweets();
+  $('#text-form').on("submit", function (event) {
       event.preventDefault();
+      if (Number($("#char-counter").val()) === 140 || $("#char-counter").val().serialize() === null) {
+      alert("Nothing to tweet");
+      return;
+    } else if (Number($("#char-counter").val()) < 0) {
+      alert("Exceeded our tweet limit")
+      return;
+    } else {
+      const newTweet = $(this).serialize();
 
-      $.post('/tweets', newTweet, function(event) {
+      $.post("/tweets", newTweet, function (event) {
         console.log(newTweet);
-
-        
-      })
-    })
-
-    const loadTweets = $(function() {
-      const $button = $('.tweet-button');
-      $button.on('click', function() {
-        $.ajax('/tweets', {method: 'GET'})
-        .then(function (morePostsHtml) {
-          console.log(morePostsHtml);
-          renderTweets(morePostsHtml);
-        })
-      })
-    })
-    //renderTweets(loadTweets())
-})
-
+      });
+    }
+  });
+});
