1567c9d3a6d8e3f22e8b527c0b35d936426e1ed7
Abhiram <abhiram.satha@gmail.com>
2021-12-16 02:29:51 +0000

    Bug fixes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,25 +6,32 @@
 const data = [];
 
 $(document).ready(function () {
-  const renderTweets = function (tweets) {
-    for (const userData of tweets) {
-      const tweetInfo = {
-        name: userData.user.name,
-        profileName: userData.user.handle,
-        avatar: userData.user.avatars,
-        dateCreated: userData.created_at,
-        text: userData.content.text,
-      };
-      $("#tweets-container").append(createTweetElement(tweetInfo));
-    }
-  };
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
+  $('#text-form').on("submit", function (event) {
+    event.preventDefault();
+   
+    if (Number($("#char-counter").val()) === 140) {
+    alert("Nothing to tweet");
+    return;
+  } else if (Number($("#char-counter").val()) < 0) {
+    alert("Exceeded our tweet limit")
+    return;
+  } else {
+    const newTweet = $(this).serialize();
+  
+
+    $.ajax({url: '/tweets', method: 'POST', data: newTweet} ).then(res => {
+      
+      loadTweets();
+    });
+
+  
+  }
+});
+
   const createTweetElement = function (tweet) {
     const { avatar, profileName, text, dateCreated, name } = tweet;
-    let $tweet = `<article class="tweet-box">
-        <header class="profile-header">
+    let $tweet = $('<article>').addClass('tweet-box')
+    let html = `        <header class="profile-header">
           <div class="profile-link">
             <img class="profile-logo" src=${avatar}/>
             <span class="profile-name">${name}</span>
@@ -40,37 +47,38 @@ $(document).ready(function () {
             <i class="fa-solid fa-heart"></i>
           </div>
         </footer>
-      </article>`;
-    /* Your code for creating the tweet element */
-    // ...
-    return $tweet;
+      `;
+      let result = $tweet.append(html);
+      return result;
+  
   }
+  const renderTweets = function(tweets) {
+    
+    const container = $('#tweets-container').html('')
 
+    for (const userData of tweets) {
+          const tweetInfo = {
+            name: userData.user.name,
+            profileName: userData.user.handle,
+            avatar: userData.user.avatars,
+            dateCreated: userData.created_at,
+            text: userData.content.text,
+          };
+          const tweetElement = createTweetElement(tweetInfo);
+          
+          container.prepend(tweetElement);
+        }
+        $('#tweet-text').val('');
+  }
   const loadTweets = function () {
 
-    $.ajax("/tweets", { method: "GET" }).then(function (morePostsHtml) {
-      renderTweets(morePostsHtml);
+    $.ajax({url: "/tweets", method: "GET" }).then(function (allTweets) {
+      renderTweets(allTweets);
     });
   };
-  loadTweets();
-  $('#text-form').on("submit", function (event) {
-      event.preventDefault();
-     
-      if (Number($("#char-counter").val()) === 140) {
-      alert("Nothing to tweet");
-      return;
-    } else if (Number($("#char-counter").val()) < 0) {
-      alert("Exceeded our tweet limit")
-      return;
-    } else {
-      const newTweet = $(this).serialize();
 
-      $.post("/tweets", newTweet, function (event) {
-      });
-      
-      $('tweets-container').replaceWith(loadTweets());
-      $('#tweet-text').val('');
-      //loadTweets();
-    }
-  });
+  loadTweets();
+ 
+
+ 
 });
